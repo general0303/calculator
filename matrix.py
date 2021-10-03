24c5ee4a0edf335cac4cefc3dc97be10c5802b50
general0303 <psh030302@gmail.com>
2021-10-03 15:32:45 +0300

    тестирование матриц

---
+++ matrix.py
@@ -0,0 +1,24 @@
+class Matrix:
+    @staticmethod
+    def addition(a, b):
+        return None
+
+    @staticmethod
+    def multiplication_on_number(a, b):
+        return None
+
+    @staticmethod
+    def multiplication(a, b):
+        return None
+
+    @staticmethod
+    def transposition(a):
+        return None
+
+    @staticmethod
+    def get_matrix_determinant(a):
+        return None
+
+    @staticmethod
+    def get_invert_matrix(a):
+        return None
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from equals import Equal
+from matrix import Matrix
 
 
 class TestEqual:
@@ -40,3 +41,23 @@ class TestEqual:
 
     def test_sinus_and_cosine(self):
         assert Equal.equal('sin(x)+cos(x)=1') == Equal.check_result('sin(x)+cos(x)', '1', 'x')
+
+
+class TestMatrix:
+    def test_addition(self):
+        assert Matrix.addition([[1, 2], [3, 4]], [[4, 3], [2, 1]]) == [[5, 5], [5, 5]]
+
+    def test_multiplication_on_number(self):
+        assert Matrix.multiplication_on_number([[1, 2], [3, 4]], 2) == [[2, 4], [6, 8]]
+
+    def test_multiplication(self):
+        assert Matrix.multiplication([[1, 2], [3, 4]], [[4, 3], [2, 1]]) == [[8, 5], [20, 13]]
+
+    def test_transposition(self):
+        assert Matrix.transposition([[1, 2], [3, 4]]) == [[1, 3], [2, 4]]
+
+    def test_determinant(self):
+        assert Matrix.get_matrix_determinant([[1, 2], [3, 4]]) == -2
+
+    def test_invert_matrix(self):
+        assert Matrix.get_invert_matrix([[1, 2], [3, 4]]) == [[4, -2], [-3, 1]]
